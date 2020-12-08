4ddc560e84d56f55261d31eb681ce46664c76b80
Hoyoung Jung <honux77@gmail.com>
2020-12-08 12:27:32 +0900

    Simple Ball 0.1

---
+++ Assets/Fall.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void nCollisionEnter(Collision collision)
+    {
+        Debug.Log("충돌 감지");
+    }
+}
---
+++ Assets/Move.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation *=
+            Quaternion.AngleAxis((float) (Input.GetAxis("Horizontal") * 30.0 * Time.deltaTime), new Vector3(0, 0, 1));
+        transform.rotation *=
+        Quaternion.AngleAxis((float)(Input.GetAxis("Vertical") * 30.0 * Time.deltaTime), new Vector3(1, 0, 0));
+    }
+}
